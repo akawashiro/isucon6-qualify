8059994eeb91c7d9d09fd14f691d56ed3f1cf98e
Akira KAWATA <akirakawata1@gmail.com>
2018-09-04 10:26:25 +0900

    Add another logger.

--- webapp/python/isuda.py
+++ webapp/python/isuda.py
@@ -294,4 +294,10 @@ if __name__ == "__main__":
     LOGGER = logging.getLogger('gunicorn.error')
     LOGGER.info('my info')
     LOGGER.debug('debug message')
+
+    gunicorn_error_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers.extend(gunicorn_error_logger.handlers)
+    app.logger.setLevel(logging.DEBUG)
+    app.logger.debug('this will show in the log')
+
     app.run()
