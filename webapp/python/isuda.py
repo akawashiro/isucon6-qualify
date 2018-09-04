959c4c3f16f439045ce15afb5dc2c5212f274d0b
Akira KAWATA <akirakawata1@gmail.com>
2018-09-04 12:49:50 +0900

    Add debug message.

--- webapp/python/isuda.py
+++ webapp/python/isuda.py
@@ -286,6 +286,7 @@ def make_keyword_list():
     cur.execute('SELECT keyword FROM entry ORDER BY CHARACTER_LENGTH(keyword) DESC')
     keywords = list()
     for k in cur.fetchall():
+        app.logger.critical('k = ' + str(k))
         keywords.append(k['keyword'])
     keywords_cache = set(keywords)
     return keywords
